dfc51754e13adcbfe7d298c64a97d0ba15544ee7
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-09-09 15:53:22 +0300

    Updates

---
+++ precache-manifest.34810ce0ec4c373850af87afccbfbb2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a9438e54ef7b84672a64eb5f95a0f3",
+    "url": "/timer-app/index.html"
+  },
+  {
+    "revision": "7cd369439397fa4147d1",
+    "url": "/timer-app/static/css/main.e6913902.chunk.css"
+  },
+  {
+    "revision": "b3909c2251a9230160ec",
+    "url": "/timer-app/static/js/2.284cf4e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/timer-app/static/js/2.284cf4e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd369439397fa4147d1",
+    "url": "/timer-app/static/js/main.c55fb4b8.chunk.js"
+  },
+  {
+    "revision": "e064554035e95a1a0191",
+    "url": "/timer-app/static/js/runtime-main.cba6f6d6.js"
+  }
+]);
