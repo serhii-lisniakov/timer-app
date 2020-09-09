d0f426dc558903ea79aef7ddcb0de9c61314257d
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-09-09 16:02:23 +0300

    push again

--- src/Timer/Timer.js
+++ src/Timer/Timer.js
@@ -98,18 +98,3 @@ export default class Timer extends Component {
         )
     }
 }
-
-
-// class InfiniteTimer extends Component{
-//     state = {
-//       time: 1000 * 1000 // ms, = 1000s
-//     }
-  
-//     onTimeEnd = () => this.setState({ time: 1000 * 1000 })
-    
-//     onTimeChange = (time) => this.setState({ time })
-  
-//     render(){
-//       return <Timer time={this.state.time} onTimeChange={this.onTimeChange} onTimeEnd={this.onTimeEnd} step={10000} autostart />
-//     }
-// }
