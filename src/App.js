cc4183998af53273d86166c183475a21b0923ab4
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-09-09 15:52:48 +0300

    timer-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Timer from './Timer/Timer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Timer
+             time={60000} // in ms
+             step={1000}
+      />
+      <Timer
+             time={5000} // in ms
+             autostart
+             step={1000} // in ms
+             infinite
+      />
+      <Timer
+             time={16000} // in ms
+             onTick={(time) => console.log('Timer III: ' + time / 1000 + ' sec is left')}
+             onTimeStart={() => console.log("Timer III запущено!")}
+             onTimePause={() => console.log("Timer III на паузі!")}
+             onTimeEnd={() => console.log("Timer III - час вийшов!")}
+             autostart
+             step={2000} // in ms
+      />
     </div>
   );
 }
---
+++ src/Timer/Timer.js
@@ -0,0 +1,115 @@
+import React, {Component} from 'react'
+import './Timer.css'
+
+
+export default class Timer extends Component {
+    state = {
+        time: this.props.time,
+        isActive: false,
+        isFinished: false,
+    }
+
+    componentDidMount = () => {
+        if (this.props.autostart) {
+            this.startTimer()
+            this.setState({isActive: true})
+        }
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval);
+        this.setState({isActive: false})
+    }
+
+    startTimer = () => {
+
+        if (this.state.isActive === false) {
+            this.setState((prevState) => {
+                return {
+                    isActive: !prevState.isActive
+                }
+            })
+
+            if (this.props.onTimeStart) this.props.onTimeStart()
+            
+            this.interval = setInterval(() => {
+
+                this.setState((prevState) => {
+                    return {
+                        time: prevState.time - this.props.step
+                    }
+                })
+    
+                if (this.state.time <= 0) {
+                    clearInterval(this.interval)
+
+                    if (this.props.infinite) {
+                        this.setState({isFinished: false, time: this.props.time,isActive: false})
+                        this.startTimer()
+                    } else {
+                        this.setState({isFinished: true, time: 0})
+                    }
+
+                    if (this.props.onTimeEnd) this.props.onTimeEnd()
+                    
+                }
+
+                if (this.props.onTick) {
+                    if (this.state.time >= 1) this.props.onTick(this.state.time)
+                }
+
+            }, this.props.step);
+    
+        } else {
+            this.setState((prevState) => {
+                return {
+                    isActive: !prevState.isActive
+                }
+            })
+            clearInterval(this.interval);
+
+            if (this.props.onTimePause) this.props.onTimePause()
+        }
+    }
+
+    msToHMinSec = (ms) => {
+        let sec = parseInt((ms / 1000) % 60),
+            min = parseInt((ms / (1000 * 60)) % 60),
+            h = parseInt((ms / (1000 * 60 * 60)) % 24);
+    
+        h = (h < 10) ? '0' + h : h;
+        min = (min < 10) ? '0' + min : min;
+        sec = (sec < 10) ? '0' + sec : sec;
+
+        return h + ':' + min + ':' + sec;
+    }
+
+    render() {
+        return (
+            <div className={this.state.isActive ? 'timer-wrapper' : 'timer-wrapper paused'}>
+                <button onClick={this.startTimer}
+                        className={this.state.isActive ? 'paused' : ''}
+                        >{this.state.isActive ? 'Pause' : 'Start'}
+                </button>
+                <span>{this.state.isFinished ? 'Finish' : this.msToHMinSec(this.state.time)}</span>
+                <div className="timer-bar-wrapper"><div className={this.props.infinite ? 'timer-bar infinite' : 'timer-bar'}
+                                                        style={{animationDuration: this.props.time+ 'ms'}}></div></div>
+            </div>
+        )
+    }
+}
+
+
+// class InfiniteTimer extends Component{
+//     state = {
+//       time: 1000 * 1000 // ms, = 1000s
+//     }
+  
+//     onTimeEnd = () => this.setState({ time: 1000 * 1000 })
+    
+//     onTimeChange = (time) => this.setState({ time })
+  
+//     render(){
+//       return <Timer time={this.state.time} onTimeChange={this.onTimeChange} onTimeEnd={this.onTimeEnd} step={10000} autostart />
+//     }
+// }
